7e94c45cbbd5b9d9f9e5072ab8201ae3ecbc68be
Nehemiah Heinz <nehemiahheinz@gmail.com>
2021-10-23 23:58:32 -0400

    bounce tweaking

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 let bird = null
 let totalDelta = 0
+let totalBounces = 0
 const VELOCITY = 300
 const WIDTH = 800
 const HEIGHT = 600
@@ -12,7 +13,7 @@ const tuna = {
   physics: {
     default: 'arcade',
     arcade: {
-      // debug: true,
+      debug: true,
       gravity: {
         y: 200
       }
@@ -38,7 +39,7 @@ function create() {
 }
 
 function update(time, delta) {
-  totalDelta += delta
+
   if (bird.body.position.x >= WIDTH - bird.width) {
     bird.body.velocity.x = -VELOCITY
   }
@@ -47,16 +48,14 @@ function update(time, delta) {
     bird.body.velocity.x = VELOCITY
   }
 
-  if (bird.body.position.y >= HEIGHT) {
-    bird.body.velocity.y = -400
+  if (bird.body.position.y >= HEIGHT - bird.height && bird.body.velocity.y > 0) {
+    bird.body.velocity.y = bird.body.velocity.y * -0.8
+    if (Math.abs(bird.body.velocity.y) < 1) { this.scene.pause(); }
   }
 
-  if (totalDelta >= 1000) {
-    console.log({gravity: bird.body.gravity.y, velocity: bird.body.velocity.y})
-
-    totalDelta = 0
+  if (totalDelta >= 100) {
+    console.log(bird.body.position.y)
   }
-
 }
 
 new Phaser.Game(tuna);
